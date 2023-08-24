035336fa74f8a382f7576c009b0c5ab415f5511c
ibmorr <jakubekk5@gmail.com>
2023-08-24 12:32:20 +0200

    Added checking if app is on deploy to log only errors from server not from local machines

--- LapkaBackend.API/Middlewares/ErrorHandlerMiddleware.cs
+++ LapkaBackend.API/Middlewares/ErrorHandlerMiddleware.cs
@@ -9,11 +9,13 @@ public class ErrorHandlerMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger _logger;
+    private readonly IWebHostEnvironment _webHostEnvironment;
 
-    public ErrorHandlerMiddleware(RequestDelegate next, ILogger logger)
+    public ErrorHandlerMiddleware(RequestDelegate next, ILogger logger, IWebHostEnvironment webHostEnvironment)
     {
         _next = next;
         _logger = logger;
+        _webHostEnvironment = webHostEnvironment;
     }
 
     public async Task Invoke(HttpContext context)
@@ -45,8 +47,11 @@ public class ErrorHandlerMiddleware
                     break;
                 default:
                     context.Response.StatusCode = 500;
+                    if (_webHostEnvironment.IsProduction())
+                    {
+                        _logger.Error(error, "server_error");
+                    }
                     errors.Add(new Error("error", "Something went wrong"));
-                    _logger.Error(error, "server_error");
                     break;
             }
             var response = context.Response;
