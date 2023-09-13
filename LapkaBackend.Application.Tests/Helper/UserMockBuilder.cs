dca54e2cc195b2f21f7022997b4505131dbe08f2
ibmorr <jakubekk5@gmail.com>
2023-09-13 09:00:37 +0200

    Added builder for user mocks

---
+++ LapkaBackend.Application.Tests/Helper/UserMockBuilder.cs
@@ -0,0 +1,48 @@
+﻿namespace LapkaBackend.Application.Tests.Helper;
+
+public class UserMockBuilder
+{
+    private readonly User _user = new ProxyGenerator().CreateClassProxy<User>();
+
+    public UserMockBuilder WithFirstName(string firstName)
+    {
+        _user.FirstName = firstName;
+        return this;
+    }
+
+    public UserMockBuilder WithLastName(string lastName)
+    {
+        _user.LastName = lastName;
+        return this;
+    }
+
+    public UserMockBuilder WithEmail(string email)
+    {
+        _user.Email = email;
+        return this;
+    }
+
+    public UserMockBuilder WithPassword(string password)
+    {
+        _user.Password = password;
+        return this;
+    }
+
+    public UserMockBuilder WithVerificationToken(string verificationToken)
+    {
+        _user.VerificationToken = verificationToken;
+        return this;
+    }
+
+    public UserMockBuilder WithRole(Role role)
+    {
+        _user.Role = role;
+        return this;
+    }
+    //If more fields are needed add them here
+
+    public User Build()
+    {
+        return _user;
+    }
+}
